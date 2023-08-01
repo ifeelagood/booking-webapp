afe5903dac24d9276eaed07f2f3c467a891c8e05
ifeelagood <thicccatto3@gmail.com>
2023-08-01 14:43:13 +1000

    fixed session expiry logic; caught verification error.

--- src/SessionManager.cpp
+++ src/SessionManager.cpp
@@ -8,13 +8,17 @@ SessionManager::SessionManager(const Poco::SharedPtr<Poco::Crypto::ECKey>& ec_ke
 
 std::string SessionManager::createSessionToken(unsigned int user_id, int is_teacher)
 {
+    Poco::Timestamp time_issued, time_expire;
+    time_expire += Poco::Timespan(6 * 60 * 60, 0);  //  TODO make expiration set by config.ini; currently 6 hours
+
+
     // create JWT
     Poco::JWT::Token token;
     token.setType("JWT");
     token.setSubject(std::to_string(user_id));
     token.payload().set("is_teacher", is_teacher);
-    token.setIssuedAt(Poco::Timestamp());
-    token.setExpiration(Poco::Timestamp() + Poco::Timespan(6 * 60 * 60, 0)); //  TODO make expiration set by config.ini; currently 6 hours
+    token.setIssuedAt(time_issued);
+    token.setExpiration(time_expire);
 
     std::string jwt;
     try
@@ -33,10 +37,20 @@ std::string SessionManager::createSessionToken(unsigned int user_id, int is_teac
 
 bool SessionManager::hasValidSignature(const std::string &jwt, Poco::JWT::Token &token)
 {
-    return signer.tryVerify(jwt, token);
+    bool success = false;
+    try
+    {
+        signer.tryVerify(jwt, token);
+        success = true;
+    }
+    catch (const Poco::JWT::SignatureVerificationException &e)
+    {
+        std::cerr << "Could not verify JWT. Reason: " << e.what() << std::endl;
+    }
+    return success;
 }
 
 bool SessionManager::hasExpired(const Poco::JWT::Token &token)
 {
-    return token.getExpiration() > Poco::Timestamp(); // short circuit
+    return token.getExpiration() < Poco::Timestamp();
 }
